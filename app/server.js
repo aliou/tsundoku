27849be33bc1306e20fc1e036ea2cc472c98827b
Aliou Diallo <code@aliou.me>
2014-11-14 15:29:38 +0100

    Make server always send angular app.

--- app/server.js
+++ app/server.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var morgan  = require('morgan');
+var path    = require('path');
 
 var app = express();
 
 app.set('port', process.env.PORT || 8000);
 
-app.get('/', function(req, res) {
-  res.send('Hello, world.');
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(morgan('combined'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/../public/index.html'))
 });
 
 app.listen(app.get('port'), function() {
