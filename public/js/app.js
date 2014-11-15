57d94e616c744d96f9327748ffa4d64bb8ba46bd
Aliou Diallo <code@aliou.me>
2014-11-15 11:30:33 +0100

    Move the Header controller in its own file.

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,5 @@
 var app = angular.module('app', ['ngRoute']);
 
-function HeaderController($scope, $location) {
-  $scope.isActive = function(viewLocation) {
-    return (viewLocation === $location.path());
-  };
-}
-
-app.controller('HeaderCtrl', HeaderController);
-
 function TsundokuRouting($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'js/home/home.html'
