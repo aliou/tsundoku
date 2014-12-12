ba5a9857f719305c3257819b3d12d6dd17b2b64f
Aliou Diallo <code@aliou.me>
2014-12-12 14:43:45 +0100

    Add goodreads module skeleton.

---
+++ app/api/books/goodreads.js
@@ -0,0 +1,7 @@
+var _       = require('underscore');
+var Book    = require('./book.model');
+var request = require('request');
+var xml2js  = require('xml2js');
+
+exports.getBook = function(book, options, callback) {
+};
