9f41f4449787be436de221f6b0d5dd0c3360d103
Aliou Diallo <code@aliou.me>
2014-12-12 12:03:39 +0100

    Add metadata fields to book model.

--- app/api/books/book.model.js
+++ app/api/books/book.model.js
@@ -4,7 +4,10 @@ var BookSchema = new mongoose.Schema({
   title:       { type: String, required: true },
   author:      { type: String, required: true },
   description: { type: String, required: true },
-  created_at:  { type: Date, default: Date.now }
+  created_at:  { type: Date, default: Date.now },
+  isbn:        { type: String, index: true },
+  length:      Number,
+  cover_url:   String
 });
 
 module.exports = mongoose.model('Book', BookSchema);
