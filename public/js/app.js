b05763e201caba74355929efc5e9b456797436e7
Aliou Diallo <code@aliou.me>
2014-12-12 11:27:30 +0100

    Route the ng book module.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,12 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'ngResource']);
 
 function TsundokuRouting($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'js/home/home.html'
+  })
+  .when('/books', {
+    templateUrl: 'js/books/book-list.html',
+    controller: 'BookListCtrl'
   });
 }
 
