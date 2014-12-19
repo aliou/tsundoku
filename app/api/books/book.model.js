9e70d3666b53a851a624e04ebea0872e252075bd
Aliou Diallo <code@aliou.me>
2014-12-19 13:52:19 +0100

    Store Goodreads id in Book model.

--- app/api/books/book.model.js
+++ app/api/books/book.model.js
@@ -2,13 +2,14 @@ var mongoose  = require('mongoose');
 var Goodreads = require('./goodreads')
 
 var BookSchema = new mongoose.Schema({
-  title:       { type: String, required: true },
-  author:      { type: String, required: true },
-  description: { type: String, required: true },
-  created_at:  { type: Date, default: Date.now },
-  isbn:        { type: String, index: true },
-  length:      Number,
-  cover_url:   String
+  title:        { type: String, required: true },
+  author:       { type: String, required: true },
+  description:  { type: String, required: true },
+  created_at:   { type: Date, default: Date.now },
+  isbn:         { type: String, index: true },
+  goodreads_id: String,
+  length:       Number,
+  cover_url:    String
 });
 
 // Add a virtual property for better access.
