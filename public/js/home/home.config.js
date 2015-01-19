0bf29c42aaa06c4e4c9afe69ed984dee5b3c6bff
Aliou Diallo <code@aliou.me>
2015-01-19 15:59:16 +0100

    Resolve the popular books in the HomeCtrl.

--- public/js/home/home.config.js
+++ public/js/home/home.config.js
@@ -1,7 +1,12 @@
 function initialConfig($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'js/home/home.html',
-    controller: 'HomeCtrl'
+    controller: 'HomeCtrl',
+    resolve: {
+      popularBooks: function(Book) {
+        return Book.popular();
+      }
+    }
   });
 
   // $routeProvider.otherwise({
