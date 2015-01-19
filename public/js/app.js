251f64d506274f248dedea5ce68048952085c961
Aliou Diallo <code@aliou.me>
2015-01-19 16:49:22 +0100

    Move navbar code in app.js.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
 var app = angular.module('app', ['ngRoute', 'ngResource']);
+
+// Navbar
+$(document).ready(function() {
+  var MQL = 1170;
+  if ($(window).width() > MQL) {
+    var headerHeight = $('.navbar-custom').height();
+    $(window).on('scroll', { previousTop: 0 }, function() {
+      var currentTop = $(window).scrollTop();
+      if (currentTop < this.previousTop) {
+        if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+          $('.navbar-custom').addClass('is-visible');
+        } else {
+          $('.navbar-custom').removeClass('is-visible is-fixed');
+        }
+      } else {
+        $('.navbar-custom').removeClass('is-visible');
+        if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
+          $('.navbar-custom').addClass('is-fixed');
+        }
+      }
+      this.previousTop = currentTop;
+    });
+  }
+});
