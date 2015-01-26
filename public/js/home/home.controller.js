a0ae01d93132cf650dab9a523573e817cc27564f
Aliou Diallo <code@aliou.me>
2015-01-26 15:47:44 +0100

    Add empty review and booklist template in homepage.

--- public/js/home/home.controller.js
+++ public/js/home/home.controller.js
@@ -1,5 +1,7 @@
 function HomeController($scope, popularBooks) {
-  $scope.popularBooks = popularBooks;
+  $scope.popularBooks   = popularBooks;
+  $scope.popularLists   = [];
+  $scope.popularReviews = [];
 }
 
 angular.module('app').controller('HomeCtrl', HomeController);
