c647b1f8838105164719f6b5833bcf6f740d6912
Aliou Diallo <code@aliou.me>
2015-01-26 17:16:53 +0100

    Improve book detail template design.

--- public/js/books/book-list.controller.js
+++ public/js/books/book-list.controller.js
@@ -3,13 +3,13 @@ var app = angular.module('app');
 
 // Define the controller.
 function BookListController($scope, books) {
-  // Store in scope the books we get from the book service.
+  // Store in scope the books we get from the book service by groups of 6.
   var byRow = 6;
   $scope.books = [];
+
   while(books.length > 0) {
     $scope.books.push(books.splice(0, byRow));
   }
-  console.log($scope.books);
 }
 
 // Save the controller in the app.
