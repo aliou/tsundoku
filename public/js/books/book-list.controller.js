48e6db3fbb20ac7322f2c6256973f252be0eee2b
Aliou Diallo <code@aliou.me>
2015-01-26 16:41:58 +0100

    Show books by row.

--- public/js/books/book-list.controller.js
+++ public/js/books/book-list.controller.js
@@ -4,7 +4,12 @@ var app = angular.module('app');
 // Define the controller.
 function BookListController($scope, books) {
   // Store in scope the books we get from the book service.
-  $scope.books = books;
+  var byRow = 6;
+  $scope.books = [];
+  while(books.length > 0) {
+    $scope.books.push(books.splice(0, byRow));
+  }
+  console.log($scope.books);
 }
 
 // Save the controller in the app.
