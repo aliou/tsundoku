72fce1af5fd243876e0bd3bda8d64b40d77a2314
Aliou Diallo <code@aliou.me>
2015-02-08 18:04:47 +0100

    Add user model.

---
+++ app/api/users/user.model.js
@@ -0,0 +1,36 @@
+var Book     = require('./../books/book.model');
+var mongoose = require ('mongoose');
+
+// TODO: Add "to read" and "read" default lists.
+// TODO: Add user custom lists.
+var UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+
+  local: {
+    email: String,
+    passowrd: String
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  },
+  twitter: {
+    id: String,
+    token: String,
+    displayName: String,
+    name: String
+  },
+});
+
+UserSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
