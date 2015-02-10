d988f6e02375be9f5ee9b2e5d3c2888341787e12
Aliou Diallo <code@aliou.me>
2015-02-10 21:09:49 +0100

    Add login modal.

--- public/js/shared/header.controller.js
+++ public/js/shared/header.controller.js
@@ -1,9 +1,22 @@
 var app = angular.module('app');
 
-function HeaderController($scope, $location) {
+function HeaderController($scope, $location, $modal) {
   $scope.isActive = function(viewLocation) {
     return (viewLocation === $location.path());
   };
+
+  $scope.loginModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'js/shared/loginModal.html',
+      controller: 'loginModalCtrl'
+    });
+  };
 }
+
+app.controller('loginModalCtrl', function($scope, $modalInstance) {
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+});
 
 app.controller('HeaderCtrl', HeaderController);
