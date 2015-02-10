2abed926e5ad202e8f2ba2f479364aed32d805f7
Shinao <rmonnerat.pro@gmail.com>
2015-02-10 15:40:29 +0100

    Add todo.

--- app/api/booklist/booklist.model.js
+++ app/api/booklist/booklist.model.js
@@ -2,6 +2,7 @@ var mongoose  = require('mongoose');
 var Book	= require('../books/book.model.js');
 
 var BookListSchema = new mongoose.Schema({
+  // TODO: Populate ?
   books:       [ { type: mongoose.Schema.Types.ObjectId, ref: 'Book' } ],
   title:       { type: String, required: true },
   description: { type: String, required: true },
