b2bce88e684596a3318a707da472709af2dfd23d
Aliou Diallo <code@aliou.me>
2015-02-11 16:36:38 +0100

    Show error message in logout modal.

--- public/js/shared/logout-modal.controller.js
+++ public/js/shared/logout-modal.controller.js
@@ -1,6 +1,20 @@
 var app = angular.module('app');
 
-function logoutModalController($scope, $modalInstance) {
+function logoutModalController($scope, $modalInstance, $http) {
+  $scope.error = null;
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+
+  $scope.logout = function() {
+    $http.get('/api/users/logout')
+    .success(function() {
+      // TODO: Auth on local side.
+      $modalInstance.close();
+    }).error(function() {
+      $scope.error = { message: 'An error occured. Please try again.' };
+    });
+  };
 };
 
 app.controller('logoutModalCtrl', logoutModalController);
