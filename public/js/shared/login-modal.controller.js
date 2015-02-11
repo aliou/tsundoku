11ab86315f03c8162f17c19ef53af7d7c1be9af5
Aliou Diallo <code@aliou.me>
2015-02-11 10:55:53 +0100

    Add quick AJAX auth.

--- public/js/shared/login-modal.controller.js
+++ public/js/shared/login-modal.controller.js
@@ -1,6 +1,21 @@
 var app = angular.module('app');
 
-function loginModalController($scope, $modalInstance) {
+function loginModalController($scope, $modalInstance, $http) {
+  $scope.username = '';
+  $scope.password = '';
+
+  $scope.submit = function() {
+    $http.post('/api/users/login', { username: $scope.username, password: $scope.password })
+    .success(function(data, status, headers, config) {
+    }).error(function(data, status, headers, config) {
+    });
+
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
 }
 
 app.controller('loginModalCtrl', loginModalController);
