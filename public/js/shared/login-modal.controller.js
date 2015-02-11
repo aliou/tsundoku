70043e25c9056e913765bda9b1ec51758fc7d873
Aliou Diallo <code@aliou.me>
2015-02-11 12:52:28 +0100

    Toggle between registration and login.

--- public/js/shared/login-modal.controller.js
+++ public/js/shared/login-modal.controller.js
@@ -1,8 +1,11 @@
 var app = angular.module('app');
 
 function loginModalController($scope, $modalInstance, $http) {
+  $scope.registering = false;
+
   $scope.username = '';
   $scope.password = '';
+  $scope.email    = '';
 
   $scope.submit = function() {
     $http.post('/api/users/login', { username: $scope.username, password: $scope.password })
@@ -15,6 +18,14 @@ function loginModalController($scope, $modalInstance, $http) {
 
   $scope.cancel = function() {
     $modalInstance.dismiss('cancel');
+  };
+
+  $scope.register = function() {
+    $http.post('/api/users/signup', { username: $scope.username, password: $scope.password, email: $scope.email })
+    .success(function(data, status, headers, config) {
+    }).error(function(data, status, headers, config) {
+    });
+    $modalInstance.close();
   };
 }
 
